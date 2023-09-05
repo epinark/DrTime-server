94f9fc3770ae7e20c0ac6d591bd7c327017c2bb3
epinark <122348695+epinark@users.noreply.github.com>
2023-09-05 23:01:28 +0200

    change createappointment funktion

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -9,8 +9,13 @@ import moment from 'moment';
 export const getAppointments = asyncHandler(async (req, res, next) => {
     try {
         console.log('getAppointments function started');
+        const {
+            userId
+        } = req.params;
 
-        const appointments = await Appointment.find()
+        const appointments = await Appointment.find({
+                userId: userId
+            })
             .populate('user', 'firstName lastName')
             .populate('doctor', 'name')
             .exec();
@@ -43,17 +48,18 @@ export const createAppointment = asyncHandler(async (req, res, next) => {
             user,
             doctor,
             appointmentdate,
+            appointmenttime,
             description
         } = req.body;
 
-
+        const [hours, minutes] = appointmenttime.split(":");
         const parsedAppointmentDate = new Date(appointmentdate);
-
+        parsedAppointmentDate.setHours(hours);
+        parsedAppointmentDate.setMinutes(minutes);
 
         if (isNaN(parsedAppointmentDate)) {
             throw new ErrorResponse("Invalid appointment date", 400);
         }
-
 
         const newAppointment = await Appointment.create({
             user,
